2b9b58b0a4f751e1016c911e2724ed52390e0cb8
Elmyare <ivanlocked55@gmail.com>
2022-05-11 09:54:33 +0700

    Create chess library

---
+++ src/libchessviz/chess.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef CHESS_H
+#define CHESS_H
+
+typedef struct {
+    int color; // 0 = black, 1 = white
+    char figure;
+} cell;
+
+typedef cell table[64];
+
+void table_print(table board);
+
+void table_fill(table board);
+
+void move(table board, const char figure_step[5]);
+
+int check_input(const char figure_step[5]);
+
+#endif
